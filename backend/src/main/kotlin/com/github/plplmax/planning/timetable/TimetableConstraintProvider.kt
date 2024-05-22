4298e5b61206e814b1fe5b163f5cca21a76f8c7a
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2024-05-22 13:22:08 +0300

    feat: add constraint to evenly distribute lessons per day

--- backend/src/main/kotlin/com/github/plplmax/planning/timetable/TimetableConstraintProvider.kt
+++ backend/src/main/kotlin/com/github/plplmax/planning/timetable/TimetableConstraintProvider.kt
@@ -2,10 +2,15 @@ package com.github.plplmax.planning.timetable
 
 import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore
 import ai.timefold.solver.core.api.score.stream.Constraint
+import ai.timefold.solver.core.api.score.stream.ConstraintCollectors.count
+import ai.timefold.solver.core.api.score.stream.ConstraintCollectors.toMap
 import ai.timefold.solver.core.api.score.stream.ConstraintFactory
 import ai.timefold.solver.core.api.score.stream.ConstraintProvider
 import ai.timefold.solver.core.api.score.stream.Joiners
 import com.github.plplmax.planning.lessons.Lesson
+import com.github.plplmax.planning.timeslots.Timeslot
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 class TimetableConstraintProvider : ConstraintProvider {
     override fun defineConstraints(constraintFactory: ConstraintFactory): Array<Constraint>? {
@@ -13,6 +18,7 @@ class TimetableConstraintProvider : ConstraintProvider {
             roomConflict(constraintFactory),
             teacherConflict(constraintFactory),
             groupConflict(constraintFactory),
+            evenlyDistributedLessonsPerDay(constraintFactory)
         )
     }
 
@@ -47,5 +53,29 @@ class TimetableConstraintProvider : ConstraintProvider {
             )
             .penalize(HardSoftScore.ONE_HARD)
             .asConstraint("Group conflict");
+    }
+
+    private fun evenlyDistributedLessonsPerDay(constraintFactory: ConstraintFactory): Constraint {
+        return constraintFactory.forEach(Lesson::class.java)
+            .groupBy({ it.timeslot?.dayOfWeek }, Lesson::group, count())
+            .concat(
+                constraintFactory.forEach(Timeslot::class.java)
+                    .groupBy(Timeslot::dayOfWeek)
+                    .join(
+                        constraintFactory.forEach(Lesson::class.java)
+                            .groupBy(Lesson::group)
+                    ).ifNotExists(
+                        Lesson::class.java,
+                        Joiners.equal(
+                            { dayOfWeek, group -> dayOfWeek to group },
+                            { lesson -> lesson.timeslot?.dayOfWeek to lesson.group })
+                    )
+            ).flattenLast { listOf(it ?: 0) }
+            .groupBy({ _, group, _ -> group }, toMap({ dayOfWeek, _, _ -> dayOfWeek }, { _, _, count -> count }))
+            .map({ group, _ -> group }, { _, dayOfWeeks -> dayOfWeeks.mapValues { entry -> entry.value.first() } })
+            .penalize(HardSoftScore.ONE_SOFT) { _, dayOfWeeks ->
+                with(dayOfWeeks) { -values.sumOf { it * it } + (values.sumOf { it } * values.sumOf { it } * 1.0 / size).roundToInt() }.absoluteValue
+            }
+            .asConstraint("Evenly distributed lessons per day")
     }
 }
