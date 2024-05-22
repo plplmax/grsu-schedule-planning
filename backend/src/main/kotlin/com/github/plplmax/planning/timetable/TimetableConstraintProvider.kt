4ed109e0f253b031a9ce766f8e6a5640137fea63
Maksim Ploski <50287455+plplmax@users.noreply.github.com>
2024-05-22 13:23:59 +0300

    feat: add constraint to remove gaps at first timeslot each day

--- backend/src/main/kotlin/com/github/plplmax/planning/timetable/TimetableConstraintProvider.kt
+++ backend/src/main/kotlin/com/github/plplmax/planning/timetable/TimetableConstraintProvider.kt
@@ -2,13 +2,13 @@ package com.github.plplmax.planning.timetable
 
 import ai.timefold.solver.core.api.score.buildin.hardsoft.HardSoftScore
 import ai.timefold.solver.core.api.score.stream.Constraint
-import ai.timefold.solver.core.api.score.stream.ConstraintCollectors.count
-import ai.timefold.solver.core.api.score.stream.ConstraintCollectors.toMap
+import ai.timefold.solver.core.api.score.stream.ConstraintCollectors.*
 import ai.timefold.solver.core.api.score.stream.ConstraintFactory
 import ai.timefold.solver.core.api.score.stream.ConstraintProvider
 import ai.timefold.solver.core.api.score.stream.Joiners
 import com.github.plplmax.planning.lessons.Lesson
 import com.github.plplmax.planning.timeslots.Timeslot
+import java.util.function.Function
 import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
@@ -18,7 +18,8 @@ class TimetableConstraintProvider : ConstraintProvider {
             roomConflict(constraintFactory),
             teacherConflict(constraintFactory),
             groupConflict(constraintFactory),
-            evenlyDistributedLessonsPerDay(constraintFactory)
+            evenlyDistributedLessonsPerDay(constraintFactory),
+            noGapsAtFirstTimeslotEachDay(constraintFactory)
         )
     }
 
@@ -77,5 +78,39 @@ class TimetableConstraintProvider : ConstraintProvider {
                 with(dayOfWeeks) { -values.sumOf { it * it } + (values.sumOf { it } * values.sumOf { it } * 1.0 / size).roundToInt() }.absoluteValue
             }
             .asConstraint("Evenly distributed lessons per day")
+    }
+
+    private fun noGapsAtFirstTimeslotEachDay(constraintFactory: ConstraintFactory): Constraint {
+        return constraintFactory.forEach(Timeslot::class.java)
+            .groupBy(Timeslot::dayOfWeek, toList())
+            .map { _, timeslots -> timeslots.first() }
+            .join(Lesson::class.java, Joiners.equal(Function.identity(), Lesson::timeslot))
+            .groupBy(
+                { timeslot, _ -> timeslot },
+                { _, lesson -> lesson.group },
+                collectAndThen(toList { _: Timeslot, lesson: Lesson -> lesson }) { it.first() }
+            ).concat(
+                constraintFactory.forEach(Timeslot::class.java)
+                    .groupBy(Timeslot::dayOfWeek, toList())
+                    .map { _, timeslots -> timeslots.first() }
+                    .join(
+                        constraintFactory.forEach(Lesson::class.java)
+                            .groupBy(Lesson::group)
+                    ).ifNotExists(
+                        Lesson::class.java,
+                        Joiners.equal(
+                            { timeslot, group -> timeslot to group },
+                            { lesson -> lesson.timeslot to lesson.group })
+                    )
+            )
+            .filter { _, _, lesson -> lesson == null }
+            .ifExists(
+                Lesson::class.java,
+                Joiners.equal(
+                    { timeslot, group, _ -> timeslot.dayOfWeek to group },
+                    { lesson -> lesson.timeslot?.dayOfWeek to lesson.group })
+            )
+            .penalize(HardSoftScore.ONE_HARD)
+            .asConstraint("No gaps at first timeslot each day")
     }
 }
